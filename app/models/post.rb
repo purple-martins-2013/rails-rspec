513863e41d75154bd7f0a9b247497ad80bb582bc
dbcapprentice <dbcapprentice@dbc.com>
2013-08-27 18:48:58 -0700

    working test for #slug in models.spec

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,13 +3,24 @@ class Post < ActiveRecord::Base
 
   scope :recent, order: "created_at DESC", limit: 5
 
-  before_save :titleize_title
+  before_save :titleize_title, :slugify
+
+  # after_save :slug
 
   validates_presence_of :title, :content
+
+  def slugify
+    self.slug = title.downcase.strip.gsub(' ', '-').gsub(/[^\w-]/, '')
+  end
+
+  
 
   private
 
   def titleize_title
     self.title = title.titleize
+    #"this is a post" => "This Is A Post"
   end
+
+  
 end
--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -1,23 +1,33 @@
 require 'spec_helper'
 
-describe Post do
-  it "title should be automatically titleized before save" do
-    pending
-  end
 
-  it "post should be unpublished by default" do
-    pending
-  end
+describe Post do
+
+  let(:title)     {"New post!"}
+  let(:content)   {"A great story"}
+  let(:post)      { Post.new(title, content) }
+
+  # it "title should be automatically titleized before save" do
+  #   pending
+  # end
+
+  # it "post should be unpublished by default" do
+  #   pending
+  # end
 
   # a slug is an automaticaly generated url-friendly
   # version of the title
-  it "slug should be automatically generated" do
-    post = Post.new
-    post.title   = "New post!"
-    post.content = "A great story"
-    post.slug.should be_nil
-    post.save
+  describe '#slug' do
+    context "with valid input" do
+      it "slug should be automatically generated" do
+        post = Post.new
+        post.title   = "New post!"
+        post.content = "A great story"
+        post.slug.should be_nil
+        post.save
 
-    post.slug.should eq "new-post"
+        post.slug.should eq "new-post"
+      end
+    end
   end
 end
